7ee39efe0a28f5ed62a88f8f04b5c1f046e9d8a9
Lcultist-code <225607393+Lcultist-code@users.noreply.github.com>
2025-11-02 06:53:27 +0000

    commit

---
+++ rps.js
@@ -0,0 +1,13 @@
+function getComputerChoice() {
+    x=Math.floor(Math.random()*3);
+    if (x<1){
+        return "rock";
+
+    }
+    else if (x<2){
+        return "paper";
+    }
+    else if (x<3){
+        return "scissors";
+    }
+}
